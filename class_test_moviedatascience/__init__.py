ef3dacb8e84b1e608dfeebbde2514cd902f8ec81
moviedatascience <moviedatascience@users.noreply.github.com>
2019-09-30 17:40:51 -0400

    added lots of stuff, think by accident

---
+++ class_test_moviedatascience/__init__.py
@@ -0,0 +1,10 @@
+"""
+class_test - a collection of helper functions
+"""
+
+import pandas as pd
+import numpy as np
+
+#sample code
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50))
