f96f71b26c3d4c3914bef1deceef653c69b00f40
moviedatascience <moviedatascience@users.noreply.github.com>
2019-10-01 19:10:21 -0400

    adding the class

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
     setuptools.setup(
     name="class_test-moviedatascience",
-    version = "0.1.1",
+    version = "0.1.2",
     author = "moviedatascience",
     description = "a collection of data science helper functions",
     long_description = LONG_DESCRIPTION,
